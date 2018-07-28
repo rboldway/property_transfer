b26d56b74b44e7c8fc1b829fdaf7cdaa72edc2f2
Richard Boldway <richard.boldway@gmail.com>
2018-07-27 19:47:41 -0500

    Remove her document, change new argument to be a document file>

--- lib/property_transfer.rb
+++ lib/property_transfer.rb
@@ -1,86 +1,71 @@
-=begin
+require "property_transfer/versdocumentn"
 
-Start line ex: Property Purchase Price
+module PropertyTransfer
 
-Section (city) line ex: Burlington
-could be multi-words
 
-Property line ex: 8023 Wheatland Road;$435,000 
-could wrap around to next line
-Regular Expression could possibly mend two lines together but
-I think its better to log the orginal lines then do a vlidation of the
-address using USPS address validation API. Once address is valid then insert into
-database.
+  class RecordTransfer
 
-=end
+    attr_accessor :input, :document, :registry, :city
 
-class PropertyTransfer
+    def initialize(document)
+      @document = document
+      @registry = {}
+      @last_line = nil
+    end
 
-  attr_accessor :input, :io, :registry, :city
+    Properties = {}
 
-  def initialize(document)
-    # replace unicode LS with ascii newline
-    @input = document.gsub(/\u2028/,"\n")
-    # puts @input
-    @io = StringIO.new @input
-    @registry = {}
-    @last_line = nil
-  end
+    def register(pattern, action)
+      registry[Regexp.new(pattern)] = action
+    end
 
-  Properties = {}
+    def perform_upon_match(line)
+      content = nil
+      key = nil
+      registry.keys.detect do |k|
+        content = line.match(k)
+        key = k
+        content
+      end
+      send(registry[key], content) if content
+      @last_line = content
+    end
 
-  def register(pattern, action)
-    registry[Regexp.new(pattern)] = action
-  end
-
-  def perform_upon_match(line)
-    content = nil
-    key = nil
-    registry.keys.detect do |k|
-      content = line.match(k)
-      key = k
+    def seek_content(pattern,document)
+      content = nil
+      document.each_line do |line|
+        break if (content = line.match(pattern))
+      end
       content
     end
-    send(registry[key], content) if content
-    @last_line = content
-  end
 
-  def seek_content(pattern,io)
-    content = nil
-    io.each_line do |line|
-      break if (content = line.match(pattern))
+    def city(content)
+      @city = content[1].to_s.chomp.squeeze(" ")
+      # puts ":city #{@city}"
     end
-    content
-  end
 
-  def city(content)
-    @city = content[1].to_s.chomp.squeeze(" ")
-    # puts ":city #{@city}"
-  end
-
-  def property(property)
-    property = property.to_a.drop(1)
-    price = property.pop
-    property[0].squeeze(" ")
-    property << @city << "WI" << price
-    puts property.join(", ")
-  end
-
-  def run
-    register(/^([0-9A-Za-z ]+)[,:; ]+\$([0-9,]+)$/, :property)
-    register(/^([A-Z a-z-]{2,})$/,:city)
-
-    puts "\nStart of prices: #{seek_content(/Property Purchase Price/, io)}"
-
-    # loop thru lines sequentiallly
-    io.each_line do |line|
-      perform_upon_match(line)
+    def property(property)
+      property = property.to_a.drop(1)
+      price = property.pop
+      property[0].squeeze(" ")
+      property << @city << "WI" << price
+      puts property.join(", ")
     end
+
+    def run
+      register(/^([0-9A-Za-z ]+)[,:; ]+\$([0-9,]+)$/, :property)
+      register(/^([A-Z a-z-]{2,})$/,:city)
+
+      puts "\nStart of prices: #{seek_content(/Property Purchase Price/, document)}"
+
+      # loop thru lines sequentiallly
+      document.each_line do |line|
+        perform_upon_match(line)
+      end
+    end
+
   end
 
 end
-
-transfer = PropertyTransfer.new document
-transfer.run
 
 
